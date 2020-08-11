55e82467280c32ffcbdc41bd385edd12984b70dc
Fran Iruela <franjid@gmail.com>
2020-08-11 18:26:14 +0200

    Added zoom ability to poster modal

--- src/app/pages/big-poster-modal/big-poster-modal.module.ts
+++ src/app/pages/big-poster-modal/big-poster-modal.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BigPosterModalPage } from './big-poster-modal';
 import { IonicModule } from "@ionic/angular";
 import { PipesModule } from "../../pipes/pipes.module";
+import { PinchZoomModule } from 'ngx-pinch-zoom';
 
 @NgModule({
   imports: [
     IonicModule,
-    PipesModule
+    PipesModule,
+    PinchZoomModule
   ],
   declarations: [
     BigPosterModalPage,
